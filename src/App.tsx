bf0dabec9fdaf8aa0cc5785e7860338c3fbab386
micahyap <yapmicahsy@gmail.com>
2024-10-27 11:29:00 +0800

    feat: add NewExpense route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './assets/components/Homepage'
+import NewExpense from './assets/components/NewExpense'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path = "/" element={<Homepage />}> </Route>
+          <Route path = "/newExpense" element = {<NewExpense />}></Route>
         </Routes>
       </BrowserRouter>
     </>
