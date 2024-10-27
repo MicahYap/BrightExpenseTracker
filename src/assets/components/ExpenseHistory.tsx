664652cefd56bb155adfaa537f6038d1e8d71659
micahyap <yapmicahsy@gmail.com>
2024-10-27 11:31:47 +0800

    feat(ExpenseHistory): plot initial layout with installation of FontAwesome for icons

---
+++ src/assets/components/ExpenseHistory.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+function ExpenseHistory() {
+ return (
+  <>
+    <div>
+      put total expense here
+    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Category</th>
+          <th>Item</th>
+          <th>Description</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><FontAwesomeIcon icon={faTrash} /></td>
+
+        </tr>
+      </tbody>
+    </table>
+  </>
+ )
+}
+
+export default ExpenseHistory
