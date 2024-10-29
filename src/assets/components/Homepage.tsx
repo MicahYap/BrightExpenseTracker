5f482eb70d1dbe30ad62597264fad50ef388db89
micahyap <yapmicahsy@gmail.com>
2024-10-29 22:33:35 +0800

    fix(Homepage): added logout function to handle logout

--- src/assets/components/Homepage.tsx
+++ src/assets/components/Homepage.tsx
@@ -1,14 +1,28 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom'
 import ExpenseHistory from "./ExpenseHistory";
+import {API_URL} from '../../constants/constants'
+import axios from "axios";
 
 function Homepage() {
  const navigate = useNavigate()
+
+ const handleLogout = async() => {
+  try{
+    await axios.delete(`${API_URL}/logout`)
+    navigate('/')
+
+  }catch (error){
+    alert('Error logging out')
+  }
+
+ }
+
  return (
   <div>
     <div>
       <span> Bright Expense Tracker </span>
-      <button>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </div>
     <div>
       <button
