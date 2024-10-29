33f3b9fbf6782a93c5c2b69a6fc6213ec5bc61c4
micahyap <yapmicahsy@gmail.com>
2024-10-29 09:27:11 +0800

    fix(Routes): fixed routes for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import Homepage from './assets/components/Homepage'
 import NewExpense from './assets/components/NewExpense'
+import AccessPage from './assets/components/AccessPage'
 
 function App() {
+  const [email, setEmail] = useState<string>('')
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path = "/" element={<Homepage />}> </Route>
+          <Route path = "/" element={<AccessPage email={email} setEmail={setEmail}/>}> </Route>
+          <Route path = "/homepage/:userId" element ={<Homepage />}></Route>
           <Route path = "/newExpense" element = {<NewExpense />}></Route>
         </Routes>
       </BrowserRouter>
