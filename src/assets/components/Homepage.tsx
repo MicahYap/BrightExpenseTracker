b4f97522c25334c22232a321c43e19356f282fe4
micahyap <yapmicahsy@gmail.com>
2024-10-30 15:23:28 +0800

    fix(Homepage): removed unused import

--- src/assets/components/Homepage.tsx
+++ src/assets/components/Homepage.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom'
 import ExpenseHistory from "./ExpenseHistory";
 import {API_URL} from '../../constants/constants'
